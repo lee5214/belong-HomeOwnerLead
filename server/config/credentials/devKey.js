7dbe480353437354758194e708e81eb589551d0a
congli <admin@lee5214.com>
2018-08-15 19:11:07 -0700

    deployment

--- server/config/credentials/devKey.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  cookieKey: "lee5214cookiejgafnmlvcxlnmvomoprefmioearsjvziasndfksdmg",
-  zillowKey: "X1-ZWz18f9udv03kb_2avji",
-  sendGridKey:
-    "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"
-};
